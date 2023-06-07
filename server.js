2ee67334c6af0a817c4cfebad73745d9a066a47e
Matt Moore <mattmoore2014@gmail.com>
2023-06-06 18:36:14 -0600

    trying to get it to seed correctly

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: process.env.SECRET,
   cookie: {
     maxAge: 300000,
     httpOnly: true,
